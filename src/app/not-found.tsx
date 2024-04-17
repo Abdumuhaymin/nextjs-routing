eab9bfce63737cb3a53a1d86523fb96bd7af1a97
uzbfamily57@gmail.com <abdurahmonov06a@gmail.com>
2024-04-17 07:43:43 +0500

    nextJS routing

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-700">
+      <h1 className="font-black text-6xl text-white">
+        404 | Hi! There is nothing here.
+      </h1>
+    </div>
+  );
+};
+
+export default NotFound;
